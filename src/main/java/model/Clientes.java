371cd1f483aca749c34f2e21d3568fb77971408e
juliesantiago <santiago.sjulie@gmail.com>
2024-07-03 10:43:01 -0300

    Adiciona primeiros arquivos do projeto, inicia mapeamento

---
+++ src/main/java/model/Clientes.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Clientes {
+    private int codigo;
+    private String nome;
+    private int telefone;
+    private String endereco;
+    private int tipoCliente;
+}
